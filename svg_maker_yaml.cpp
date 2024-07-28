98cf579e9f2d7d39576c588e12dee5f0d94e8497
Yulai202020 <ngmyulai@gmail.com>
2024-07-28 16:45:20 +0400

    first commit

--- svg_maker_yaml.cpp
+++ svg_maker_yaml.cpp
@@ -8,6 +8,21 @@
 #include <string>
 #include <format>
 #include <map>
+#include <iterator>
+
+std::string join(const std::vector<std::string>& vec, const std::string& separator) {
+    std::ostringstream result;
+    std::copy(vec.begin(), vec.end(), std::ostream_iterator<std::string>(result, separator.c_str()));
+    
+    // Convert the stream to a string and remove the last separator
+    std::string joinedString = result.str();
+    if (!joinedString.empty()) {
+        joinedString.erase(joinedString.length() - separator.length());
+    }
+
+    return joinedString;
+}
+
 
 template <typename... Args>
 void format_tags(std::vector<std::string>& tags, const std::string fmto, Args... args) {
@@ -18,7 +33,7 @@ void format_tags(std::vector<std::string>& tags, const std::string fmto, Args...
 
 int main(int argc, char** argv) {
     if (argc != 3) {
-        return;
+        return 1;
     }
 
     for (int i = 0; i < argc; i++) {
@@ -49,7 +64,41 @@ int main(int argc, char** argv) {
             std::string format = "<rect x=\"{}\" y=\"{}\" height=\"{}\" width=\"{}\" style=\"{}\"/>";
             format_tags(tags, format, x, y, height, width, style);
         } else if (key == "circle") {
+            int cx = std::stoi(value["cx"]);
+            int cy = std::stoi(value["cy"]);
+            int radius = std::stoi(value["r"]);
 
+            std::string format = "<circle cx=\"{}\" cy=\"{}\" r=\"{}\" style=\"{}\"/>";
+            format_tags(tags, format, cx, cy, radius, style);
+        } else if (key == "ellipse") {
+            int cx = std::stoi(value["cx"]);
+            int cy = std::stoi(value["cy"]);
+            int radius_x = std::stoi(value["rx"]);
+            int radius_y = std::stoi(value["ry"]);
+
+            std::string format = "<ellipse cx=\"{}\" cy=\"{}\" rx=\"{}\" ry=\"{}\" style=\"{}\"/>";
+            format_tags(tags, format, cx, cy, radius_x, radius_y, style);
+        } else if (key == "line") {
+            int x1 = std::stoi(value["x1"]);
+            int y1 = std::stoi(value["y1"]);
+
+            int x2 = std::stoi(value["x2"]);
+            int y2 = std::stoi(value["y2"]);
+
+            std::string format = "<ellipse x1=\"{}\" y1=\"{}\" x2=\"{}\" y2=\"{}\" style=\"{}\"/>";
+            format_tags(tags, format, x1, y1, x2, y2, style);
+        } else if (key == "polygon") {
+            std::vector<std::string> points = config["key"]["points"].as<std::vector<std::string>>();
+            std::string joined = join(points, " ");
+
+            std::string format = "<polygon points=\"{}\" style=\"{}\"/>";
+            format_tags(tags, format, joined, style);
+        } else if (key == "polyline") {
+            std::vector<std::string> points = config["key"]["points"].as<std::vector<std::string>>();
+            std::string joined = join(points, " ");
+
+            std::string format = "<polyline points=\"{}\" style=\"{}\"/>";
+            format_tags(tags, format, joined, style);
         }
     }
 
